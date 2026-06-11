76868502e89a7b9eacfc7573d35f812f662542cd
Chris <33391598+Mouky66@users.noreply.github.com>
2026-06-11 08:41:10 +0200

    Create user_setups.h

---
+++ user_setups/user_setups.h
@@ -0,0 +1,21 @@
+#ifndef USER_SETUP_H
+#define USER_SETUP_H
+
+#define ST7796_DRIVER 1
+#define TFT_WIDTH 320
+#define TFT_HEIGHT 480
+
+// Display Pins
+#define TFT_MISO 19
+#define TFT_MOSI 23
+#define TFT_SCLK 18
+#define TFT_CS   5
+#define TFT_DC   21
+#define TFT_RST  22
+#define TFT_BL   27
+#define TFT_BACKLIGHT_ON 1
+
+// Touch Pins
+#define TOUCH_CS 12
+
+#endif
